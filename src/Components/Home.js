c0535a52afbb69ff90d60d31add47c7cf1e47ec8
ShruthaGandla <shrutaradhika@gmail.com>
2018-01-09 21:57:01 -0500

    added a comment in Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+//testing the merge conflict
 import PropTypes from 'prop-types';
 //this file is to show how the age attribute when incremented is not rendered with the new value.that is why we use this.state property.
 export class Home extends React.Component{
